97dc56d56a459192cc569d41e5449dea72a497b0
Andre Fonseca <aoqfonseca@gmail.com>
2014-08-23 16:34:20 -0300

    First test for healthcheck handle

---
+++ tests/test_servers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from twitter_watcher import server
+
+class ServerTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.api = server.api.test_client()
+
+    def test_healthcheck(self):
+        response = self.api.get('/healthcheck')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.data, 'WORKING')
