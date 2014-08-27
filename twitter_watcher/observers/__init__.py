6d5eab172a1288476cabe2875d9aeceed7724ace
Andre Fonseca <aoqfonseca@gmail.com>
2014-08-27 12:04:25 -0300

    Refactoring: creating files in observers

--- twitter_watcher/observers/__init__.py
+++ twitter_watcher/observers/__init__.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
 
 
-class ObserverTwitter(object):
-	""" Listener/Observer for new tweet event """
-
-	def on_message(self, tweet):
-		pass
-
-	def run(self):
-		pass
+__all__ = ['twitter'] 
