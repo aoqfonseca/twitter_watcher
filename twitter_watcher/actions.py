21b6cf4b2b36caebb6268a300b0a84649db907a0
Andre Fonseca <aoqfonseca@gmail.com>
2014-08-27 19:18:51 -0300

    Rename runners to actions

---
+++ twitter_watcher/actions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import requests
+
+
+class Callback(object):
+
+	def send(self):
+		url = self.url_callback()
+		json = self.json_data()
+		requests.post(url, data=json)
+
+	def url_callback(self):
+		raise NotImplementedError()
+
+	def json_data(self):
+		raise NotImplementedError()
--- twitter_watcher/observers/__init__.py
+++ twitter_watcher/observers/__init__.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+import requests
+
+__all__ = ['twitter']
 
 
-__all__ = ['twitter'] 
+class CallBack(object):
+
+    def run(self):
+        url = self.url_callback()
+        json = self.json_data()
+        requests.post(url, data=json)
+        pass
+
+    def json_data(self):
+        raise NotImplementedError()
+
+    def url_callback(self):
+        raise NotImplementedError()
